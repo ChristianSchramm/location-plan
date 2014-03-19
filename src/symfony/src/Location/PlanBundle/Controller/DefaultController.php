466d73abf8a7238656560779fb9727c8248a0e61
Tobias Dietrich (Selen) <info@etooby.com>
2014-03-19 10:51:51 +0100

    [Formatiert]

--- src/symfony/src/Location/PlanBundle/Controller/DefaultController.php
+++ src/symfony/src/Location/PlanBundle/Controller/DefaultController.php
@@ -1,20 +1,17 @@
 <?php
-
 namespace Location\PlanBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
     /**
      * @Route("/", name="home")
      * @Route("")
      * @Template()
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         return array();
     }
 }
--- src/symfony/src/Location/PlanBundle/Controller/RoomController.php
+++ src/symfony/src/Location/PlanBundle/Controller/RoomController.php
@@ -1,11 +1,9 @@
 <?php
-
 namespace Location\PlanBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class RoomController extends Controller
-{
+class RoomController extends Controller {
 }
