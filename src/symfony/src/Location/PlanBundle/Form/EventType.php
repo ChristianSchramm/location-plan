a8a48f4c89d27613baf289d21bf2ed23e6d6fa60
Tobias Dietrich (Selen) <info@etooby.com>
2014-03-21 10:21:38 +0100

    form

---
+++ src/symfony/src/Location/PlanBundle/Form/EventType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Location\PlanBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EventType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('from')
+            ->add('until')
+            ->add('leader')
+            ->add('coleader')
+            ->add('room')
+            ->add('assets')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Location\PlanBundle\Entity\Event'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'location_planbundle_event';
+    }
+}
