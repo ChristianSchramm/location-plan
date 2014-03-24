0e20533c40075250e58cd0da024ed110ea824778
Tom Kopp <witzbould@gmx.de>
2014-03-24 20:34:19 +0100

    ExportController final with exceptions

--- src/symfony/src/TouchMe/FloorPlanBundle/Controller/ExportController.php
+++ src/symfony/src/TouchMe/FloorPlanBundle/Controller/ExportController.php
@@ -16,9 +16,16 @@ class ExportController extends Controller
 
         $events = $em->getRepository('TouchMeFloorPlanBundle:Event')->findAll();
         $assets = $em->getRepository('TouchMeFloorPlanBundle:Asset')->findAll();
-        // Absolut path
+        // Absolute paths
         $webPath = $this->get('kernel')->getRootDir() . '/../web/';
-        $uploadPath = $webPath . '/upload/';
+        $uploadPath = $webPath . 'upload/';
+
+        if (is_dir($webPath . 'zip')) {
+            $zipPath = $webPath . 'zip/';
+        } else {
+            mkdir($webPath . 'zip/');
+            $zipPath = $webPath . 'zip/';
+        }
 
         $eventArray = array();
         foreach($events as $event)
@@ -26,45 +33,47 @@ class ExportController extends Controller
             $eventArray[] = $event->toArray();
         }
 
-        // Delete old ZipArchiv
-        if(is_file($filename))
-        {
-            unlink($filename);
-        }
-
-        // Create ZipArchive
+        // Create ZipArchive or overwrite old one
         $zipArchiv = new \ZipArchive();
-        if ($zipArchiv->open($webPath . 'zip/' . $filename, \ZipArchive::CREATE) !== TRUE)
+        if ($zipArchiv->open($zipPath . $filename, \ZipArchive::CREATE | \ZIPARCHIVE::OVERWRITE) === TRUE)
+        {
+            // Create JSON-File
+            $zipArchiv->addFromString('events.json', json_encode($eventArray));
+
+            // Add related files
+            foreach ($assets as $asset)
+            {
+                if(is_file($uploadPath . $asset->getSrc()))
+                {
+                    $zipArchiv->addFile($uploadPath . $asset->getSrc(), $asset->getSrc());
+                }
+            }
+
+            if ($zipArchiv->close())
+            {
+                //$response = new Response();
+                $response = new Response(readfile($zipPath . $filename));
+
+                // Set headers
+                $response->headers->set('Cache-Control', 'private');
+                $response->headers->set('Content-type', mime_content_type($zipPath . $filename));
+                $response->headers->set('Content-Disposition', 'attachment; filename="' . basename($filename) . '"');
+                $response->headers->set('Content-length', filesize($zipPath . $filename));
+
+                // Send headers before outputting anything
+                $response->sendHeaders();
+                return $response;
+            }
+            else
+            {
+                throw $this->createNotFoundException('Zip file could not be written.');
+            }
+        }
+        else
         {
             throw $this->createNotFoundException('Zip file could not be created.');
         }
 
-        // Create JSON-File
-        $zipArchiv->addFromString('events.json', json_encode($eventArray));
 
-        // Add related files
-        foreach ($assets as $asset)
-        {
-            if(is_file($uploadPath.$asset->getSrc()))
-            {
-                $zipArchiv->addFile($uploadPath.$asset->getSrc(), $asset->getSrc());
-            }
-        }
-
-        $zipArchiv->close();
-
-        $this->getRequest()->getUriForPath($filename);
-
-        $response = new Response();
-
-        // Set headers
-        $response->headers->set('Cache-Control', 'private');
-        $response->headers->set('Content-type', mime_content_type($filename));
-        $response->headers->set('Content-Disposition', 'attachment; filename="' . basename($filename) . '"');
-        $response->headers->set('Content-length', filesize($filename));
-
-        // Send headers before outputting anything
-        $response->sendHeaders();
-        return $response->setContent(readfile($filename));
     }
 }
