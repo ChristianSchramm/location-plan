aa4205940b880c3da5a16010e2d2dff1d1c4481c
Christian Schramm <christian-schramm83@gmx.de>
2014-03-24 14:43:19 +0100

    [Task] first css styles

--- src/html/assets/js/scripts.js
+++ src/html/assets/js/scripts.js
@@ -2,24 +2,24 @@ $( document ).ready(function() {
 
 });
   function showList(i, elem){
-        $(".map-container").removeClass("fullscreen");
-        if($(elem).hasClass("active")){
-           $(elem).removeClass("active"); 
-        }else{
-            $(".buttonIcon").removeClass("active");
-            $(elem).addClass("active");
-        }
-        $("#listWrap").addClass("active");
-
-        if($(".list"+i).hasClass("active")){
-            $(".list"+i).removeClass("active");
-        }else{
-            $(".list").removeClass("active");
-            setTimeout(function(){$(".list"+i).addClass("active")}, 200)
-
-        }
-        if(!($(".buttonIcon").hasClass("active"))){
-            $("#listWrap").removeClass("active");
-            $(".map-container").addClass("fullscreen");
-        }
+    $(".map-container").removeClass("fullscreen");
+    if($(elem).hasClass("active")){
+       $(elem).removeClass("active"); 
+    }else{
+        $(".ico").removeClass("active");
+        $(elem).addClass("active");
     }
+    $("#flap").addClass("active");
+
+    if($(".list"+i).hasClass("active")){
+        $(".list"+i).removeClass("active");
+    }else{
+        $(".list").removeClass("active");
+        setTimeout(function(){$(".list"+i).addClass("active")}, 200)
+
+    }
+    if(!($(".ico").hasClass("active"))){
+        $("#flap").removeClass("active");
+        $(".map-container").addClass("fullscreen");
+    }
+}
