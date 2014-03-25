eb413c9176b74ade9f287103f65b25b1463d9f11
Tobias Dietrich (Selen) <info@etooby.com>
2014-03-25 09:53:06 +0100

    [TASK] location form

--- src/symfony/src/TouchMe/FloorPlanBundle/Controller/LocationController.php
+++ src/symfony/src/TouchMe/FloorPlanBundle/Controller/LocationController.php
@@ -1,14 +1,18 @@
 <?php
-
 namespace TouchMe\FloorPlanBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use TouchMe\FloorPlanBundle\Form\LocationType;
+use TouchMe\FloorPlanBundle\Entity\Location;
 
-class LocationController extends Controller
-{
-    public function indexAction()
-    {
+class LocationController extends Controller {
+    public function indexAction() {
         $locations = $this->getDoctrine()->getRepository('TouchMeFloorPlanBundle:Location')->findAllOrderedByNumber();
         return $this->render('TouchMeFloorPlanBundle:Location:index.html.twig', array('locations' => $locations));
+    }
+    
+    public function newAction() {
+        $form = $this->createForm(new LocationType(), new Location());
+        return $this->render('TouchMeFloorPlanBundle:Location:new.html.twig', array('form' => $form->createView()));
     }
 }
---
+++ src/symfony/src/TouchMe/FloorPlanBundle/Form/LocationType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace TouchMe\FloorPlanBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class LocationType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('number')
+            ->add('type')
+            ->add('description')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TouchMe\FloorPlanBundle\Entity\Location'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'touchme_floorplanbundle_location';
+    }
+}
