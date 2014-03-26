774fd4a81e9479881be3b6fdb7d958ad7e90ee5e
Tobias Dietrich (Selen) <info@etooby.com>
2014-03-26 08:42:44 +0100

    [TASK] edit and remove room

--- src/symfony/src/TouchMe/FloorPlanBundle/Controller/LocationController.php
+++ src/symfony/src/TouchMe/FloorPlanBundle/Controller/LocationController.php
@@ -6,35 +6,72 @@ use TouchMe\FloorPlanBundle\Form\LocationType;
 use TouchMe\FloorPlanBundle\Entity\Location;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class LocationController extends Controller 
-{
+class LocationController extends Controller {
     public function indexAction() {
         $locations = $this->getDoctrine()->getRepository('TouchMeFloorPlanBundle:Location')->findAllOrderedByNumber();
         return $this->render('TouchMeFloorPlanBundle:Location:index.html.twig', array('locations' => $locations));
     }
     
-    public function newAction() 
-    {
-        $form = $this->createForm(new LocationType(), new Location(), array(
-    'action' => $this->generateUrl('location_create')));
+    public function newAction() {
+        $form = $this->createForm(new LocationType(), new Location(), array('action' => $this->generateUrl('location_create')));
         return $this->render('TouchMeFloorPlanBundle:Location:new.html.twig', array('form' => $form->createView()));
     }
-
-    public function createAction() 
-    {
-      $form = $this->createForm(new LocationType(), new Location());
-      $form->bind($this->getRequest());
-
-      $location = $form->getData();
-
-      if ($form->isValid()){
+    
+    public function createAction() {
+        $form = $this->createForm(new LocationType(), new Location());
+        $form->bind($this->getRequest());
+        
+        $location = $form->getData();
+        
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($location);
+            $em->flush();
+            
+            return $this->redirect($this->generateUrl('location'));
+        }
+        return $this->render('TouchMeFloorPlanBundle:Location:new.html.twig', array('form' => $form->createView()));
+    }
+    
+    public function removeAction($id) {
         $em = $this->getDoctrine()->getManager();
-        $em->persist($location);
-        $em->flush();
+        $location = $em->getRepository('TouchMeFloorPlanBundle:Location')->findOneById($id);
+        if ($location) {
+            $em->remove($location);
+            $em->flush();
+        }
         
         return $this->redirect($this->generateUrl('location'));
-      }
-      return $this->render('TouchMeFloorPlanBundle:Location:new.html.twig', array('form' => $form->createView()));
-
+    }
+    
+    public function editAction($id) {
+        $location = $this->getDoctrine()->getRepository('TouchMeFloorPlanBundle:Location')->findOneById($id);
+        if ($location) {
+            $form = $this->createForm(new LocationType(), $location, array('action' => $this->generateUrl('location_save', array('id' => $location->getId()))));
+        }
+        
+        return $this->render('TouchMeFloorPlanBundle:Location:edit.html.twig', array('form' => $form->createView()));
+    }
+    
+    public function saveAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $location = $em->getRepository('TouchMeFloorPlanBundle:Location')->findOneById($id);
+        
+        if ($location) {
+            $form = $this->createForm(new LocationType(), $location);
+            $form->bind($this->getRequest());
+            
+            $location = $form->getData();
+            
+            if ($form->isValid()) {
+                
+                $em->persist($location);
+                $em->flush();
+                
+                return $this->redirect($this->generateUrl('location'));
+            }
+            return $this->render('TouchMeFloorPlanBundle:Location:edit.html.twig', array('form' => $form->createView()));
+        }
+        return $this->redirect($this->generateUrl('location'));
     }
 }
