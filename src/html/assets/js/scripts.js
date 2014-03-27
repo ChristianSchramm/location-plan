b598d0ce46cb41965d33330d5034a470d738ac5a
NemareLethiere <soeren.schreier@online.de>
2014-03-27 01:36:28 +0100

    tooltip css animation script für clickfunktion und schliessen der tooltips einfügen weiterer testtooltips zum test der ausrichtungsklassen

--- src/html/assets/js/scripts.js
+++ src/html/assets/js/scripts.js
@@ -1,25 +1,47 @@
 $( document ).ready(function() {
 
 });
-  function showList(i, elem){
-    $(".map-container").removeClass("fullscreen");
-    if($(elem).hasClass("active")){
-       $(elem).removeClass("active"); 
-    }else{
-        $(".ico").removeClass("active");
-        $(elem).addClass("active");
-    }
-    $("#flap").addClass("active");
+function showList(i, elem){
+  $(".map-container").removeClass("fullscreen");
+  if($(elem).hasClass("active")){
+    $(elem).removeClass("active"); 
+  }else{
+    $(".main-nav .ico").removeClass("active");
+    $(elem).addClass("active");
+  }
+  $("#flap").addClass("active");
 
-    if($(".list"+i).hasClass("active")){
-        $(".list"+i).removeClass("active");
-    }else{
-        $(".list").removeClass("active");
-        setTimeout(function(){$(".list"+i).addClass("active")}, 200)
-
-    }
-    if(!($(".ico").hasClass("active"))){
-        $("#flap").removeClass("active");
-        $(".map-container").addClass("fullscreen");
-    }
+  if($(".list"+i).hasClass("active")){
+    $(".list"+i).removeClass("active");
+  }else{
+    $(".list").removeClass("active");
+    setTimeout(function(){$(".list"+i).addClass("active")}, 200)
+  }
+  if(!($(".main-nav .ico").hasClass("active"))){
+    $("#flap").removeClass("active");
+    $(".map-container").addClass("fullscreen");
+  }
 }
+
+function showTooltip(elem){
+  closeTooltip($(".tooltip.active .close"));
+  $(elem).parent(".room").css("z-index", "2");
+  $(elem).siblings(".tooltip").toggleClass("active");
+  $(elem).toggleClass("active");
+  setTimeout(function(){$(elem).css("z-index", "1");}, 200);
+}
+function closeTooltip(elem){
+  
+  var ico = $(elem).parent(".tooltip").siblings(".room .ico");
+  ico.css("z-index", "2");
+  setTimeout(function(){
+    ico.toggleClass("active");
+    $(elem).parent(".tooltip").toggleClass("active");
+   }, 100);
+   setTimeout(function(){
+    $(elem).parents(".room").css("z-index", "");
+   }, 200);
+
+  
+}
+
